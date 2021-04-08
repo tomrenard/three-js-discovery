35fad540b0bfac0118f08911e036f1f7d4cb39f4
Tom Renard <tom.renard@zoho.com>
2021-04-08 17:13:34 +0200

    try 3js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useMemo } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import * as THREE from "three";
+import './App.css'
 
-function App() {
+import five from "./assets/five.png";
+
+const Box = (props) => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+
+  // Set up state for the hovered and active state
+  const [active, setActive] = useState(false);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  });
+
+  const texture = useMemo(() => new THREE.TextureLoader().load(five), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh
+    {...props}
+    ref={mesh}
+    scale={active ? [2, 2, 2] : [1.5, 1.5, 1.5]}
+    onClick={(e) => setActive(!active)}
+      >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial attach="material" transparent side={THREE.DoubleSide}>
+        <primitive attach="map" object={texture} />
+      </meshBasicMaterial>
+    </mesh>
+  );
+}
+
+const App = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[2.5, 0, 0]} />
+    </Canvas>
   );
 }
 
